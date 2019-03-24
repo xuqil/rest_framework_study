34cd2005073f6966858fccbd6f98f17baaefdfd4
xuqil <13829842679@163.com>
2019-03-25 00:34:32 +0800

    更正url

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,5 +5,5 @@ app_name = 'snippets'
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
-    path('snippets/(P<pk>[0-9]+)/', views.snippet_detail),
+    path('snippets/<int:pk>/', views.snippet_detail),
 ]
