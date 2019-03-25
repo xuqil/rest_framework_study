5d828d4a7870691a170a382f0102efd852952da6
xuqil <13829842679@163.com>
2019-03-25 16:02:08 +0800

    使用 Routers（有个无法解决的问题）

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,35 +1,14 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from snippets import views
-from rest_framework import renderers
 
 app_name = 'snippets'
+# 创建路由器并注册我们的视图。
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet, base_name='user')
 
-urlpatterns = format_suffix_patterns([
-    path('', views.api_root),
-    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
-])
-
-snippet_list = views.SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-snippet_detail = views.SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = views.SnippetViewSet.as_view({
-    'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-user_list = views.UserViewSet.as_view({
-    'get': 'list'
-})
-user_detail = views.UserViewSet.as_view({
-    'get': 'retrieve'
-})
+# API URL 现在由路由器自动确定。
+urlpatterns = [
+    path('', include(router.urls)),
+]
