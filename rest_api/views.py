e875a52464b1760086b19a73d74d5566c996ae10
xuqil <13829842679@163.com>
2019-03-27 23:57:51 +0800

    csrf认证(方法一)

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import HttpResponse
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django.utils.decorators import method_decorator
 from rest_framework.views import APIView  # APIView视图继承View视图
 from rest_framework.authentication import BasicAuthentication
 from rest_framework import exceptions
 
 
-class StudentsView(APIView):
+class StudentsView(View):
     """
     APIView视图会根据请求识别meth方法，自动分配执行函数
     最先执行的是dispatch方法
     """
+    @method_decorator(csrf_protect)  # 对class视图进行免除csrf验证，不能对单独方法进行修饰
     def dispatch(self, request, *args, **kwargs):
         # 访问该class时必须先执行该函数，可以充当修饰器
         ret = super(StudentsView, self).dispatch(request, *args, **kwargs)
