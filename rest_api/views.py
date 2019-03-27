d0029f5249b862db380821ca0c37fba4128239ee
xuqil <13829842679@163.com>
2019-03-28 00:42:23 +0800

    APIView自定义认证

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import HttpResponse
-from django.views import View
-from django.views.decorators.csrf import csrf_exempt, csrf_protect
-from django.utils.decorators import method_decorator
 from rest_framework.views import APIView  # APIView视图继承View视图
 from rest_framework.authentication import BasicAuthentication
 from rest_framework import exceptions
 
 
-@method_decorator(csrf_exempt, name='dispatch')
-class StudentsView(View):
+class StudentsView(APIView):
     """
     APIView视图会根据请求识别meth方法，自动分配执行函数
     最先执行的是dispatch方法
     """
+    authentication_classes = [BasicAuthentication, ]  # 自定义配置文件
 
     def get(self, request, *args, **kwargs):
+        self.dispatch
+        print(request)  # 该request不是原request
+        ret = {
+            'code': 1000,  # code可以任意设置
+            'msg': 'xxx'
+        }
         return HttpResponse("GET")
 
     def post(self, request, *args, **kwargs):
