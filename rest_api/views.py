6be242109714282a0ea7782ff4e0531a3cc99d88
xuqil <13829842679@163.com>
2019-03-27 23:11:17 +0800

    View视图源码分析（dispatch）

--- rest_api/views.py
+++ rest_api/views.py
@@ -7,6 +7,10 @@ class StudentsView(View):
     View视图会根据请求识别meth方法，自动分配执行函数
     最先执行的是dispatch方法
     """
+    def dispatch(self, request, *args, **kwargs):
+        # 访问该class时必须先执行该函数，可以充当修饰器
+        ret = super(StudentsView, self).dispatch(request, *args, **kwargs)
+        return ret
 
     def get(self, request, *args, **kwargs):
         return HttpResponse("GET")
